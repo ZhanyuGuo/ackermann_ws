dbbf62f98257dc2b5afd214ba4e3abaabdc23657
Guo_Zhanyu <942568052@qq.com>
2023-05-15 15:12:34 +0800

    add gif generate

--- src/matlab/mainLQR.m
+++ src/matlab/mainLQR.m
@@ -13,12 +13,12 @@ global params;
 params.wheelbase = 0.335;
 params.v_max = 1.0;
 params.w_max = pi / 3;
-params.Q = 1 * eye(3);
+params.Q = 50 * eye(3);
 params.R = 1 * eye(2);
 params.dt = 0.1;
 
 %% init state
-X0 = [-2, 0, -pi / 2]; % init state: [x(m), y(m), theta(rad)]
+X0 = [-2, 0, -pi / 4]; % init state: [x(m), y(m), theta(rad)]
 U0 = [0, 0]; % init control: [v(m/s), w(rad)]
 
 %% trajectory
@@ -46,7 +46,7 @@ while count < 150
         X_plot = plot(X(1), X(2), '-r', 'LineWidth', 1.5);
         car_plot = plot(X(1), X(2), 'or');
         ref_plot = plot(X_d(idx, 1), X_d(idx, 2), 'ob');
-        xlabel("x(m)"); ylabel("y(m)"); title("path tracking");
+        xlabel("x(m)"); ylabel("y(m)"); title("path tracking (LQR)");
         legend('reference', 'actual', 'car');
 
         subplot(1, 3, 2); hold on;
@@ -58,6 +58,12 @@ while count < 150
         xlabel("time(s)"); ylabel("front steer angle(rad)"); title("steer angle");
 
         vis_init = true;
+
+        % gif
+        frame = getframe(gcf);
+        im = frame2im(frame);
+        [I, map] = rgb2ind(im, 128);
+        imwrite(I, map, 'lqr.gif', 'LoopCount', Inf, 'DelayTime', params.dt);
     else
         subplot(1, 3, 1); hold on;
         set(X_plot, 'XData', [get(X_plot, 'XData') X(1)]);
@@ -74,6 +80,12 @@ while count < 150
         subplot(1, 3, 3); hold on;
         set(w_plot, 'XData', [get(w_plot, 'XData') t]);
         set(w_plot, 'YData', [get(w_plot, 'YData') U(2)]);
+
+        % gif
+        frame = getframe(gcf);
+        im = frame2im(frame);
+        [I, map] = rgb2ind(im, 128);
+        imwrite(I, map, 'lqr.gif', 'WriteMode', 'append', 'DelayTime', params.dt);
     end
 
     drawnow;
